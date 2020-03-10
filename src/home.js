286e5fae820e89d874cdbd2b9d6e8376b29ac472
Luis J. Rodriguez O <ljavierrodriguez@gmail.com>
2020-03-10 11:55:39 -0300

    Footer Component

--- src/home.js
+++ src/home.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import Navbar from './components/navbar';
+import Footer from './components/footer';
 
 const Home = (props) => {
     return (
         <>
-        <Navbar />
-        <div className="container">
-            <div className="row">
-                <div className="col-md-12 d-flex justify-content-center">
-                    <i className="fab fa-html5 fa-5x"></i>
+            <Navbar />
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12 d-flex justify-content-center">
+                        <i className="fab fa-html5 fa-5x"></i>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-12 d-flex justify-content-center">
+                        <p className="text-success">Welcome to Webpack, Bootstrap and ReactJS</p>
+                    </div>
                 </div>
             </div>
-            <div className="row">
-                <div className="col-md-12 d-flex justify-content-center">
-                    <p className="text-success">Welcome to Webpack, Bootstrap and ReactJS</p>
-                </div>
-            </div>
-        </div>
+            <Footer />
         </>
     )
 }
