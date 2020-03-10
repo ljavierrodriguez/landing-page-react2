521f842048da1185061a84e268848d95e68077e3
Luis J. Rodriguez O <ljavierrodriguez@gmail.com>
2020-03-10 11:05:51 -0300

    Landing Page React

---
+++ src/home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Home = (props) => {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-md-12 d-flex justify-content-center">
+                    <i className="fab fa-html5 fa-5x"></i>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-md-12 d-flex justify-content-center">
+                    <p className="text-success">Welcome to Webpack, Bootstrap and ReactJS</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Add css files
+import 'bootstrap/dist/css/bootstrap.min.css';
+//import 'font-awesome/css/font-awesome.min.css'; // 4.x
+import '@fortawesome/fontawesome-free/css/all.css'; // 5.x
+
+// Custom CSS
+import './index.css';
+
+// Add js files
+import 'jquery';
+import 'popper.js';
+import 'bootstrap';
+
+import Home from './home';
+
+ReactDOM.render(<Home />, document.querySelector("#root"));
